5ac0fa94f5b4296d1538e8b04e43dbd9f83e0fd5
Sigfredo Olmedo <sigfredoolmedo6c17@gmail.com>
2022-05-04 17:59:00 -0500

    Add initial commit

---
+++ ordinariomain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   ordinariomain.c
+ * Author: sigfr
+ *
+ * Created on 4 de mayo de 2022, 05:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
